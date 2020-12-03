4c02346677a6b4ff6d58a17aadd235ec2649760a
malcolm26 <30697907+malcolm26@users.noreply.github.com>
2020-12-03 11:52:26 -0600

    Create Grades.java
    testing github file

---
+++ Grades.java
@@ -0,0 +1,19 @@
+package scanner;
+
+public class Grades 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\t////////////////\\\\\\\\\\\\\\\\\\\\");
+		System.out.println("\t==\tStudent Points\t ==");
+		System.out.println("\t\\\\\\\\\\\\\\\\\\\\\\\t//////////////");
+		System.out.println();
+		System.out.println("\tName\t\tLab\t\tBonus\t\tTotal");
+		System.out.println("\t----\t\t---\t\t-----\t\t-----");
+		System.out.println("\tMatt\t\t"+"43\t\t"+"10\t\t"+(43+10));
+		System.out.println("\tJames\t\t"+"53\t\t"+"9\t\t"+(53+9));
+		System.out.println("\tHeisnberg\t"+"95\t\t"+"5\t\t"+(95+5));
+		System.out.println("\tMarian\t\t"+"72\t\t"+"9\t\t"+(72+9));
+		System.out.println("\tAbigal\t\t"+"64\t\t"+"3\t\t"+(64+3));
+	}
+}
